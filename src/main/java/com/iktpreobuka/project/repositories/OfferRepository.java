b9e493c68283639c9f1cae9e1a81ea69a25e5eb1
bakastrumpf <baka.strumpf@gmail.com>
2023-04-10 22:31:08 +0200

    query added

--- src/main/java/com/iktpreobuka/project/repositories/OfferRepository.java
+++ src/main/java/com/iktpreobuka/project/repositories/OfferRepository.java
@@ -2,12 +2,14 @@ package com.iktpreobuka.project.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.iktpreobuka.project.entities.OfferEntity;
 
 public interface OfferRepository extends CrudRepository <OfferEntity, Integer>{
 	
-	public List<OfferEntity> findByPriceBetween(double lowerPrice, double upperPrice);
+	@Query("select o from OfferEntity o where o.regularPrice < :upperPrice and o.regularPrice > :lowerPrice")
+	public List<OfferEntity> findByPriceBetween(Double lowerPrice, Double upperPrice);
 
 }
