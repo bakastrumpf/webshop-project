c6364faafef753a4f34e57fe86c5399d77efd10a
bakastrumpf <baka.strumpf@gmail.com>
2023-04-25 21:54:08 +0200

    Category service impl added

--- src/main/java/com/iktpreobuka/project/services/CategoryServiceImpl.java
+++ src/main/java/com/iktpreobuka/project/services/CategoryServiceImpl.java
@@ -1,38 +1,39 @@
 package com.iktpreobuka.project.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.iktpreobuka.project.repositories.CategoryRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.iktpreobuka.project.entities.CategoryEntity;
+//import com.iktpreobuka.project.repositories.CategoryRepository;
 
 public interface CategoryServiceImpl extends CategoryService {
 	 
-	@Autowired
-	private OfferService offerService;
-	
-	@Autowired
-	private BillService billService;
-	
-	@Autowired 
-	private CategoryRepository categoryRepository;
-	
-	@Override
-	public CategoryEntity deleteCategory(Integer id) {
-		
-		// VP 
-		
-		CategoryEntity category = categoryRepository.findById(id).orElse(null);
-		
-		boolean checkOffers = offerService.hasCategoryNonExpirationOffers(id);
-		boolean checkBills = billService.areActiveBills(id);
-		
-		if(category == null || checkOffers || checkBills) {
-			return null;
-		}
-		
-		categoryRepository.delete(category);
-		
-		return category;
-		
-	}
+//	@Autowired
+//	private OfferService offerService;
+//	
+//	@Autowired
+//	private BillService billService;
+//	
+//	@Autowired 
+//	private CategoryRepository categoryRepository;
+//	
+//	@Override
+//	public CategoryEntity deleteCategory(Integer id) {
+//		
+//		// VP 
+//		
+//		CategoryEntity category = categoryRepository.findById(id).orElse(null);
+//		
+//		boolean checkOffers = offerService.hasCategoryNonExpirationOffers(id);
+//		boolean checkBills = billService.areActiveBills(id);
+//		
+//		if(category == null || checkOffers || checkBills) {
+//			return null;
+//		}
+//		
+//		categoryRepository.delete(category);
+//		
+//		return category;
+//		
+//	}
 
 }
