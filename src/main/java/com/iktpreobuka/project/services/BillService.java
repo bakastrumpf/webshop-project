a20312a83f34413a6c2f870325e946aa68ffe019
bakastrumpf <baka.strumpf@gmail.com>
2023-05-05 00:12:27 +0200

    bill service extended

--- src/main/java/com/iktpreobuka/project/services/BillService.java
+++ src/main/java/com/iktpreobuka/project/services/BillService.java
@@ -1,5 +1,8 @@
 package com.iktpreobuka.project.services;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.iktpreobuka.project.entities.BillEntity;
 
 public interface BillService {
@@ -9,5 +12,15 @@ public interface BillService {
 	public BillEntity updateBill(Integer id, BillEntity bill);
 
 	boolean areActiveBills(Integer categoryId);
+
+	boolean activeBills();
+
+	boolean areBillsActiveByCategory(Integer categoryId);
+
+	public List<BillEntity> getAllBillsByDate(LocalDate startDate, LocalDate endDate);
+
+	public List<BillEntity> findAllByOfferCategoryId(Integer categoryId);
+
+	public List<BillEntity> findAllBillsByBuyer(Integer buyerId);
 
 }
