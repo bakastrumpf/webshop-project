798bf17dad06b4121b797d5db46cc8c4a8cc2479
baka.strumpf <baka.strumpf@gmail.com>
2024-02-21 18:33:59 +0100

    bureaucratic commit

--- src/main/java/com/iktpreobuka/project/repositories/BillRepository.java
+++ src/main/java/com/iktpreobuka/project/repositories/BillRepository.java
@@ -10,12 +10,11 @@ import com.iktpreobuka.project.entities.BillEntity;
 	// TODO 3.2 u paketu com.iktpreobuka.project.repositories napraviti interfejs BillRepository
 
 public interface BillRepository extends CrudRepository <BillEntity, Integer> {
-	
+
+	public List<BillEntity> findAllByUserId(Integer userId);
 	public List<BillEntity> findByBuyerId(Integer buyerId);
-	
 	public List<BillEntity> findByOfferCategoryId(Integer categoryId);
 	public List<BillEntity> findAllByOfferCategoryId(Integer categoryId);
-	
 	public List<BillEntity> findByBillCreatedBetween(LocalDate startDate, LocalDate endDate);
 
 }
